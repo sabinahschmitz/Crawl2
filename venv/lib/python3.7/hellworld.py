e604c6295e6bfa2b79dc3ed653e45814b0428227
Sabs <sabinahschmitz@gmail.com>
2019-02-05 22:52:37 +0100

    changed a comment2

--- venv/lib/python3.7/hellworld.py
+++ venv/lib/python3.7/hellworld.py
@@ -1,4 +1,3 @@
-"""This is supposed tlaishjdaojsdaöojsdalösdjaslöjlajsdljaösdjaöjd"""
 
 """Script to gather news outlet IP's and return highest ranking of countries mentioned."""
 from bs4 import BeautifulSoup
@@ -7,6 +6,7 @@ import requests
 from collections import Counter
 import csv
 import numpy as np
+import pandas as pd
 
 #create the soup
 url = 'https://sabinaschmitz.com/test-page/'
@@ -20,31 +20,15 @@ country_list = [content.text for content in soup.select("[class^='sqs-block-cont
 
 #count the country name mentions in country list
 country_count = Counter(country_list)
-for key, value in country_count.items():
-   print(key, value)
+df = pd.DataFrame.from_dict(country_count, orient='index').reset_index()
+df = df.rename(columns={'index':'country name', 0:'count'})
+
+
+export_csv = df.to_csv(r'/Users/sabinaschmitz/PycharmProjects/Helloworld/dataframe.csv', index=False)
+print(df)
 
 
 
-country_keys = np.array(country_count.keys(), dtype=('U25'))
-count_vals = np.array(country_count.values(), dtype=str)
-website = str(url)
-
-#create csv file countries-per-blog
-f = csv.writer(open('countries-per-blog2.csv', 'w'))
-#heading of top row
-f.writerow(['Country', 'Count', 'Website'])
-
-
-f.writerow([country_keys, count_vals, website])
-
-
-
-
-#path = 'path/to/a/csvfile'
-#r is used for only reading(opening) the file
-#read_csv = open(path,'r')
-#r+ is used for reading and writing(opening)to the same file
-#readwrite_csv = open(path, 'r+')
 
 def main():
     """Main entry point for the script."""
