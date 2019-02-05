9d4c278e655298c04bf261e4747767d8b85b9225
Sabs <sabinahschmitz@gmail.com>
2019-02-05 22:57:41 +0100

    Simplified Code and used Pandas Dataframe that is stored to a local csv file

--- venv/lib/python3.7/hellworld.py
+++ venv/lib/python3.7/hellworld.py
@@ -18,12 +18,15 @@ soup = BeautifulSoup(page.content, 'html.parser')
 #putting the soup output into country_list
 country_list = [content.text for content in soup.select("[class^='sqs-block-content'] p")]
 
-#count the country name mentions in country list
+#Counter counts the amounts of mentiones in the dictionary
 country_count = Counter(country_list)
+#creates a dataframe out of the counted objects
 df = pd.DataFrame.from_dict(country_count, orient='index').reset_index()
+#renames the colums of the dataframe
 df = df.rename(columns={'index':'country name', 0:'count'})
 
 
+#this exports the dataframe to a csv file
 export_csv = df.to_csv(r'/Users/sabinaschmitz/PycharmProjects/Helloworld/dataframe.csv', index=False)
 print(df)
 
